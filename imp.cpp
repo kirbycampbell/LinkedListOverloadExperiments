f364200b37ebfffc26520a29d93b14a1e5602fe2
kirbycampbell <jkirbycampbell@gmail.com>
2019-06-05 16:31:01 -0700

    Working on The Linked Virtual Pointer List

--- imp.cpp
+++ imp.cpp
@@ -10,15 +10,15 @@ ArtistTypes::~ArtistTypes() {
 }
 
 Artist::Artist() {
-	name = nullptr;
+ name = nullptr;
 	rating = 0;
 }
 
 Artist::Artist(char nameArg[], double ratingArg) {
 	int length; // variable declaration for determining length of arg.
 	length = strlen(nameArg);
+	name = new char[length + 1];
 	strcpy(name, nameArg);
-
 	rating = ratingArg;
 }
 
@@ -32,9 +32,41 @@ const double Artist::getRating() {
 
 void ArtistTypes::addAnArtist() {
 	cout << "Add an artist" << endl;
+	char tName[arraySize];
+	double tRating;
+	node * curr;
+	node * newNode;
+	
+
+	cout << "Enter an artist name: " << endl;
+	cin.getline(tName, arraySize, '\n');
+	cin.clear(); //Input stream clear
+	cout << "Enter a rating: " << endl;
+	cin >> tRating;	
+	cin.clear(); //Input stream clear
+	Artist tempArtist(tName, tRating);
+	if (head == nullptr) {
+		head = new node;
+		head->artist = tempArtist;
+	}
+	else if (head != nullptr) {
+		curr = head->link;
+		while (curr->link != nullptr) {
+			curr = curr->link;
+		}
+		if (curr->link == nullptr) {
+			newNode = new node;
+			newNode->artist = tempArtist;
+			curr->link = newNode;
+		}
+	}
+
+	//CONFUSION HERE AND IN OTHER PROJ
 }
 
 
 void ArtistTypes::listArtists() {
 	cout << "List the Artists" << endl;
+	cout << head->artist.getName() << endl; 
+	cout << head->artist.getRating() << endl;
 }
--- mainTest.cpp
+++ mainTest.cpp
@@ -6,6 +6,37 @@ int main()
 
 	//::::::::::::::::::::: Welcome and Load Routine:::::::::::::::::::::::::::::::::
 	cout << "Welcome to the Test app!" << endl;
+	ArtistTypes artistDB;
+	bool cont = true;
+	char sent;
 
+	while (cont) {
+		cin.clear(); //Input stream clear
+	
+		cout << "Want to add an artist or List the artists? (Enter A or L or Q for quit)" << endl;
+		cin.clear(); //Input stream clear
+
+		cin.get(sent);
+		cin.ignore(100, '\n');
+		cin.clear(); //Input stream clear
+
+	
+		if (sent == 'a') {
+			artistDB.addAnArtist();
+
+		}
+		else if (sent == 'l') {
+			artistDB.listArtists();
+
+		}
+		else if (sent == 'q') {
+			break;
+			cont = false;
+		}
+		else {
+			cout << "Bad.  Enter something better." << endl;
+		}
+	}
+	cout << "Closing Program" << endl;
 	return 0;
 }
